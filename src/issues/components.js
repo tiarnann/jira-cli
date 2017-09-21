29cb4e8264f8fd14e122b5cd1b9a52adde905276
Tíarnán <tiarnan@plynk.me>
2017-09-21 20:57:47 +0100

    Moving files. Adding more component methods for listing and selection

--- src/issues/components.js
+++ src/issues/components.js
@@ -7,11 +7,36 @@ import opn from 'opn';
 
 // Local
 import jira from '../jira';
+import Projects from '../projects';
 import Issues from './issues'
 
 export default class JiraComponents {
     constructor(){
         this.issues = new Issues
+        this.projects = new Projects
+    }
+
+    listComponents(issueId){
+        return jira.api.findIssue( issueId ).then(function( issue ){
+            let project = issue.fields.project.key
+            return jira.api.listComponents(project).then(function(components){
+                //console.log(components)
+                let list = components.map(o => o.name).reduce((a,o)=>{
+                    if(a.indexOf(o) ==  -1){
+                        a.push(o);
+                    }
+                    
+                    return a;
+                },[])
+                return list
+            }).catch(function( res ){
+                jira.showErrors( res );
+                process.exit();
+            });
+		}).catch(function( res ){
+			jira.showErrors( res );
+			process.exit();
+		});
     }
 
 	createUpdateObject(action, name){
@@ -55,6 +80,25 @@ export default class JiraComponents {
         inquirer.prompt(question).then(function( res ) {
             let action = res.action.toLowerCase()
             _this.performComponentAction(action, args, component)
+        });
+    }
+
+    async askForComponent(args){
+        const listOfAvailabeComponents = await this.listComponents(args)
+        // console.log(listOfAvailabeComponents)
+        var question = [
+            {
+             type: 'list',
+             name: 'component',
+             message: 'Available Components: ',
+             choices: listOfAvailabeComponents
+            }
+        ];
+
+        let _this = this
+        inquirer.prompt(question).then(function( res ) {
+            let component = res.component
+            _this.askForComponentAction(args, component)
         });
     }
 
